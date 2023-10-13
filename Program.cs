ef1a8a2234572df883a64890617920eddfc79b3b
Abhinav Kumar <kabhinav713@gmail.com>
2023-10-13 16:35:28 +0530

    [Abhinav]-Adding CSVReadWrite part

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace TPLProjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("# Read data from CSV & Write data in CSV");
+            csvHandler.ImplementCSVDataHandling();
+            Console.WriteLine();
+        }
+    }
+}
---
+++ csvHandler.cs
@@ -0,0 +1,48 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPLProjects
+{
+    public class csvHandler
+    {
+        public static void ImplementCSVDataHandling()
+        {
+            string importFilePath = @"C:\Users\dell\source\repos\TPLProjects\TPLProjects\addresses.csv";
+            string exportFilePath = @"C:\Users\dell\source\repos\TPLProjects\TPLProjects\export.csv";
+
+            //reading csv file
+
+            using(var reader = new StreamReader(importFilePath))
+            using(var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var records = csv.GetRecords<AddressData>().ToList();
+                Console.WriteLine("Read data successfully from addresses csv.");
+                foreach(AddressData addressData in records)
+                {
+                    Console.WriteLine("\t" + addressData.firstname);
+                    Console.WriteLine("\t" + addressData.lastname);
+                    Console.WriteLine("\t" + addressData.address);
+                    Console.WriteLine("\t" + addressData.city);
+                    Console.WriteLine("\t" + addressData.state);
+                    Console.WriteLine("\t" + addressData.code);
+                    Console.Write("\n");
+
+
+                }
+                Console.WriteLine("\n*********Now reading from csv file and write to csv file*********");
+
+                //writing csv file
+                using (var writer = new StreamWriter(exportFilePath))
+                using (var csvExport = new CsvWriter(writer, CultureInfo.InvariantCulture))
+                {
+                    csvExport.WriteRecords(records);
+                }
+            }
+        }
+    }
+}
