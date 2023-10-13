cefb6eb84818e3db81602687f56acd52530a5076
Abhinav Kumar <kabhinav713@gmail.com>
2023-10-13 17:16:22 +0530

    [Abhinav]-Adding CSVToJsonReadWrite part

--- Program.cs
+++ Program.cs
@@ -4,9 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("# Read data from CSV & Write data in CSV");
-            csvHandler.ImplementCSVDataHandling();
-            Console.WriteLine();
+            //Console.WriteLine("# Read data from CSV & Write data in CSV");
+            //csvHandler.ImplementCSVDataHandling();
+            //Console.WriteLine();
+
+            Console.WriteLine("# Read data from CSV & Write data in JSON");
+            ReadCSV_And_WriteJSON.ImplementCSVToJSON();
         }
     }
 }
---
+++ ReadCSV_And_WriteJSON.cs
@@ -0,0 +1,51 @@
+﻿using CsvHelper;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace TPLProjects
+{
+    public class ReadCSV_And_WriteJSON
+    {
+        public static void ImplementCSVToJSON()
+        {
+            string importFilePath = @"C:\Users\dell\source\repos\TPLProjects\TPLProjects\addresses.csv";
+            string exportFilePath = @"C:\Users\dell\source\repos\TPLProjects\TPLProjects\export.json";
+
+            //reading csv file
+
+            using (var reader = new StreamReader(importFilePath))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var records = csv.GetRecords<AddressData>().ToList();
+                Console.WriteLine("Read data successfully from addresses data.");
+                foreach (AddressData addressData in records)
+                {
+                    Console.WriteLine("\t" + addressData.firstname);
+                    Console.WriteLine("\t" + addressData.lastname);
+                    Console.WriteLine("\t" + addressData.address);
+                    Console.WriteLine("\t" + addressData.city);
+                    Console.WriteLine("\t" + addressData.state);
+                    Console.WriteLine("\t" + addressData.code);
+                    Console.Write("\n");
+
+
+                }
+                Console.WriteLine("\n*********Now reading from csv file and write to json file*********");
+
+                //writing data to json file
+                Newtonsoft.Json.JsonSerializer serializer = new Newtonsoft.Json.JsonSerializer();
+                using (StreamWriter sw = new StreamWriter(exportFilePath))
+                using (JsonWriter writer = new JsonTextWriter(sw))
+                {
+                    serializer.Serialize(writer, records);
+                }
+            }
+        }
+    }
+}
